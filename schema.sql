7b85f08d255b50a02250629991b21b111bb5a322
MikeFogz <MikeFogz@gmail.com>
2021-01-04 22:57:03 -0800

    fixed PRIMARY KEY on employee table

--- schema.sql
+++ schema.sql
@@ -4,9 +4,10 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY NOT NULL,
+  id INT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   song VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id NULL
+  manager_id NULL,
+  PRIMARY KEY (id)
 );
