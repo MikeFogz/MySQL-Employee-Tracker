0b1f7ab4de85e22bc9934889d846e50dd1897673
MikeFogz <MikeFogz@gmail.com>
2021-01-05 15:20:57 -0800

    Added first inquirer prompt for adding employee, FIXED employee table (forgot last_name)

--- app.js
+++ app.js
@@ -7,3 +7,35 @@ const connection = mysql.createConnection({
   password: "password",
   database: "employee_db",
 });
+
+// const start
+
+
+const addEmployee = () => {
+    inquirer
+    .prompt([
+        { 
+            name: 'firstName',
+            type: 'input',
+            message: 'What is the first name of the new employee?',
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'What is the last name of the new employee?',
+        },
+        {
+            name: 'role',
+            type: 'input',
+            message: `What is the employee's role?`,
+        },
+        {
+            name: 'manager',
+            type: 'input',
+            message: `Who is their manager, if they have one?`,
+        },  
+        ])
+
+// const viewEmployee
+
+// const updateRole
