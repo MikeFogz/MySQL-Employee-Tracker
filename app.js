1ae967a0315c3e887f55d01adedb901f60c34052
MikeFogz <MikeFogz@gmail.com>
2021-01-06 22:49:02 -0800

    Updated: Finished addRole function

--- app.js
+++ app.js
@@ -151,40 +151,38 @@ const addDep = () => {
         inquirer
         .prompt([
             { 
-                name: 'firstName',
+                name: 'title',
                 type: 'input',
-                message: 'What is the first name of the new employee?',
+                message: 'What is the title for the new role?',
             },
             {
-                name: 'lastName',
+                name: 'salary',
                 type: 'input',
-                message: 'What is the last name of the new employee?',
+                message: 'What is the salary for the new role?',
             },
             {
-                name: 'role',
+                name: 'department',
                 type: 'input',
-                message: `What is the employee's role?`,
+                message: `What department is the role in?`,
             },
             {
-                name: 'manager',
+                name: 'id',
                 type: 'input',
-                message: `Who is their manager, if they have one?`,
+                message: `What is the ID for this role?`,
             },  
             ])
             .then((answer) => {
-                // when finished prompting, insert a new item into the db with that info
                 connection.query(
-                'INSERT INTO employee SET ?',
+                'INSERT INTO role SET ?',
                 {
-                    first_name: answer.firstName,
-                    last_name: answer.lastName,
-                    role_id: answer.role,
-                    manager_id: answer.manager
+                    title: answer.title,
+                    salary: answer.salary,
+                    department_id: answer.department,
+                    id: answer.id
                 },
                 (err) => {
                     if (err) throw err;
-                    console.log('Employee successfully added!');
-                    // re-prompt the user for if they want to add, view, or update.
+                    console.log('Role successfully added!');
                     start();
                 }
                 );
