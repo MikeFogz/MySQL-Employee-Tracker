e0d9b71f571a009ae0c69ec2723cc24dccbf3d16
Mike Fogarty <MikeFogz@gmail.com>
2021-04-05 21:45:38 -0700

    UPDATE: Added foreign & refence keys to schema

--- schema.sql
+++ schema.sql
@@ -10,18 +10,24 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL (6,2) NOT NULL,
   department_id INT NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id),
+  REFERENCES department (id),
+  ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id), 
+  FOREIGN KEY (role_id)
+  REFERENCES role (id)
+  ON DELETE CASCADE
 );
